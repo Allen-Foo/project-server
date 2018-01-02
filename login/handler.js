b2f7da85387cd89c7a416ea8c2e300af8edd63e5
stevenwong <steven@thedartsfactory.com>
2018-01-02 09:40:48 +0800

    [login] login function

--- login/handler.js
+++ login/handler.js
@@ -2,6 +2,7 @@
 const APIResponseLoginModel = require('../apiResponseModel/APIResponseLoginModel');
 const User = require('../entity/User');
 const ServerConstant = require("../common/ServerConstant");
+const Utilities = require('../common/Utilities');
 
 module.exports.login = (event, context, callback) => {
 
@@ -9,5 +10,25 @@ module.exports.login = (event, context, callback) => {
 
   var response = new APIResponseLoginModel();
 
-  callback(null, response);
+  // check awsId valid
+  if (!event.awsId.trim()) {
+    response.statusCode = ServerConstant.API_CODE_ACC_NOT_LINKED_AWS_ID;
+    callback(null, response);
+  }
+
+  User.findFirst('awsId = :awsId', {':awsId' : event.awsId}, function(err, user) {
+    if (err) callback(err, null);
+    	if (user != null) {
+	      Utilities.bind(user, response);
+	      response.statusCode = ServerConstant.API_CODE_OK;
+	      callback(null, response);
+	    }
+      else {
+      	response.statusCode = ServerConstant.API_CODE_TARGET_USER_NOT_FOUND;
+      	callback(null, response);
+    	}
+    });
+    
+    
+
 };
