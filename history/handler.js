6968b937789f68197fc1f81b405fe6d1e28a9fc0
Allen <allen@thedartsfactory.com>
2018-03-12 16:27:48 +0800

    [remove-bookmark] add remove from bookmark api

--- history/handler.js
+++ history/handler.js
@@ -47,3 +47,48 @@ module.exports.addToBookmark = (event, context, callback) => {
     }
   });
 };
+
+module.exports.removeFromBookmark = (event, context, callback) => {
+  // get data from the body of event
+  const data = event.body;
+  const userId = event.userId;
+
+  const classId = event.path.id;
+  // get user profile by using awsId
+
+  var response = new ApiResponseUserModel();
+
+  // check awsId valid
+  if (!data.userId) {
+    response.statusCode = ServerConstant.API_CODE_ACC_NOT_LINKED_AWS_ID;
+    callback(null, response);
+    return;
+  }
+
+  User.findFirst('userId = :userId', {':userId' : data.userId}, function(err, user) {
+    if (err) {
+      callback(err, null);
+      return;
+    } 
+    if (user != null) {
+      Utilities.bind(user, response);
+      // remove the class id
+      user.bookmark = user.bookmark.filter(clsId => clsId !== classId)
+      user.updatedAt = Utilities.getCurrentTime();
+      user.saveOrUpdate(function(error, res) {
+        if (error) {
+          callback(error, null);
+          return;
+        }
+        response.statusCode = ServerConstant.API_CODE_OK;
+        Utilities.bind(res, response);
+        callback(null, response);
+      });
+    }
+    else {
+      response.statusCode = ServerConstant.API_CODE_TARGET_USER_NOT_FOUND;
+      callback(null, response);
+      return;
+    }
+  });
+};
