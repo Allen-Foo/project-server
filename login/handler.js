6655d0abc54c014783dcf94f4da4d5e8abadc0e3
Allen <allen@thedartsfactory.com>
2018-06-20 11:58:04 +0800

    [apidoc] add apidoc json and some documentation

--- login/handler.js
+++ login/handler.js
@@ -4,6 +4,61 @@ const User = require('../entity/User');
 const ServerConstant = require("../common/ServerConstant");
 const Utilities = require('../common/Utilities');
 
+
+/**
+ * @api {post} /login Login
+ * @apiName login
+ * @apiGroup Login
+ *
+ * @apiParam {String} awsId Users awsId.
+ *
+ * @apiSuccess {String} userId userId of the User.
+ * @apiSuccess {String} companyId  companyId of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *        "statusCode": 200,
+ *        "userId": "21bc0ea1-0a09-4c1c-a782-a1bd42f68b31",
+ *        "maintenanceSchedule": "",
+ *        "email": "null",
+ *        "username": "test create tutor",
+ *        "name": "null",
+ *        "website": "null",
+ *        "phone": "null",
+ *        "address": "null",
+ *        "isTutor": false,
+ *        "skill": "null",
+ *        "introduction": "null",
+ *        "awsId": "null",
+ *        "loginType": "null",
+ *        "googleId": "null",
+ *        "facebookId": "null",
+ *        "avatarUrl": "null",
+ *        "bookmark": [],
+ *        "totalRatings": 0,
+ *        "userRole": "tutor",
+ *        "gold": 0,
+ *        "freeGold": 0,
+ *        "companyId": "49eaeb42-bc5f-4dae-a45c-0dfbfda5231c"
+ *      }
+ *
+ * @apiError NotLinkedAwsId The id of the aws was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 401 Not linked to aws id
+ *     {
+ *       "error": "API_CODE_ACC_NOT_LINKED_AWS_ID"
+ *     }
+ *     
+ * @apiError NotLinkedAwsId Cannot find the user.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 202 Target user not found
+ *     {
+ *       "error": "API_CODE_TARGET_USER_NOT_FOUND"
+ *     }
+ */
 module.exports.login = (event, context, callback) => {
   // get data from the body of event
   const data = event.body;
--- tutor/handler.js
+++ tutor/handler.js
@@ -7,6 +7,54 @@ const APIResponseUserModel = require('../apiResponseModel/APIResponseUserModel')
 const APIResponseTutorListModel = require('../apiResponseModel/APIResponseTutorListModel');
 const Utilities = require('../common/Utilities');
 
+
+/**
+ * @api {post} /createTutor Create Tutor
+ * @apiName CreateTutor
+ * @apiGroup Tutor
+ *
+ * @apiParam {String} userId Company's unique ID (userId).
+ * @apiParam {Object} tutorData the tutor information for the new tutor
+ *
+ * @apiSuccess {String} userId userId of the User.
+ * @apiSuccess {String} companyId  companyId of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *        "statusCode": 200,
+ *        "userId": "21bc0ea1-0a09-4c1c-a782-a1bd42f68b31",
+ *        "maintenanceSchedule": "",
+ *        "email": "null",
+ *        "username": "test create tutor",
+ *        "name": "null",
+ *        "website": "null",
+ *        "phone": "null",
+ *        "address": "null",
+ *        "isTutor": false,
+ *        "skill": "null",
+ *        "introduction": "null",
+ *        "awsId": "null",
+ *        "loginType": "null",
+ *        "googleId": "null",
+ *        "facebookId": "null",
+ *        "avatarUrl": "null",
+ *        "bookmark": [],
+ *        "totalRatings": 0,
+ *        "userRole": "tutor",
+ *        "gold": 0,
+ *        "freeGold": 0,
+ *        "companyId": "49eaeb42-bc5f-4dae-a45c-0dfbfda5231c"
+ *      }
+ *
+ * @apiError NotAuthorizied The id of the company user was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 401 Not Authorized
+ *     {
+ *       "error": "NotAuthorizied"
+ *     }
+ */
 module.exports.createTutor = (event, context, callback) => {
   // get data from the body of event
   const data = event.body;
@@ -44,6 +92,54 @@ module.exports.createTutor = (event, context, callback) => {
   });
 };
 
+
+/**
+ * @api {post} /updateTutor Update Tutor
+ * @apiName updateTutor
+ * @apiGroup Tutor
+ *
+ * @apiParam {String} userId Users unique ID.
+ * @apiParam {Object} data the tutor information
+ *
+ * @apiSuccess {String} userId userId of the User.
+ * @apiSuccess {String} companyId  companyId of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *        "statusCode": 200,
+ *        "userId": "21bc0ea1-0a09-4c1c-a782-a1bd42f68b31",
+ *        "maintenanceSchedule": "",
+ *        "email": "null",
+ *        "username": "test create tutor",
+ *        "name": "null",
+ *        "website": "null",
+ *        "phone": "null",
+ *        "address": "null",
+ *        "isTutor": false,
+ *        "skill": "null",
+ *        "introduction": "null",
+ *        "awsId": "null",
+ *        "loginType": "null",
+ *        "googleId": "null",
+ *        "facebookId": "null",
+ *        "avatarUrl": "null",
+ *        "bookmark": [],
+ *        "totalRatings": 0,
+ *        "userRole": "tutor",
+ *        "gold": 0,
+ *        "freeGold": 0,
+ *        "companyId": "49eaeb42-bc5f-4dae-a45c-0dfbfda5231c"
+ *      }
+ *
+ * @apiError NotAuthorizied The id of the tutor was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 401 Not Authorized
+ *     {
+ *       "error": "NotAuthorizied"
+ *     }
+ */
 module.exports.updateTutor = (event, context, callback) => {
   // get data from the body of event
   const data = event.body;
@@ -76,9 +172,41 @@ module.exports.updateTutor = (event, context, callback) => {
   });
 };
 
+
+/**
+ * @api {post} /getTutorList Get Tutor List
+ * @apiName getTutorList
+ * @apiGroup Tutor
+ *
+ * @apiParam {String} userId Users unique ID.
+ *
+ * @apiSuccess {Number} statusCode statusCode of response.
+ * @apiSuccess {String} companyId  companyId of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *        "statusCode": 200,
+ *        "tutorList": []
+ *      }
+ *
+ * @apiError NotAuthorizied The id of the tutor was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 401 Not Authorized
+ *     {
+ *       "error": "NotAuthorizied"
+ *     }
+ */
 module.exports.getTutorList = (event, context, callback) => {
   // get data from the body of event
   const data = event.body;
+
+  if (!data.userId) {
+    response.statusCode = ServerConstant.API_CODE_INVALID_PARAMS;
+    callback(null, response);
+    return;
+  }
 
   let response = new APIResponseTutorListModel();
   let lastEvaluatedKey = data && data.lastStartKey ?  {tutorId: data.lastStartKey} : null;
@@ -100,6 +228,52 @@ module.exports.getTutorList = (event, context, callback) => {
   })
 };
 
+/**
+ * @api {post} /deleteTutor Delete Tutor
+ * @apiName deleteTutor
+ * @apiGroup Tutor
+ *
+ * @apiParam {String} userId tutor unique ID.
+ *
+ * @apiSuccess {Number} statusCode statusCode of response.
+ * @apiSuccess {String} companyId  companyId of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *        "statusCode": 200,
+ *        "userId": "21bc0ea1-0a09-4c1c-a782-a1bd42f68b31",
+ *        "maintenanceSchedule": "",
+ *        "email": "null",
+ *        "username": "test create tutor",
+ *        "name": "null",
+ *        "website": "null",
+ *        "phone": "null",
+ *        "address": "null",
+ *        "isTutor": false,
+ *        "skill": "null",
+ *        "introduction": "null",
+ *        "awsId": "null",
+ *        "loginType": "null",
+ *        "googleId": "null",
+ *        "facebookId": "null",
+ *        "avatarUrl": "null",
+ *        "bookmark": [],
+ *        "totalRatings": 0,
+ *        "userRole": "tutor",
+ *        "gold": 0,
+ *        "freeGold": 0,
+ *        "companyId": "49eaeb42-bc5f-4dae-a45c-0dfbfda5231c"
+ *     }
+ *
+ * @apiError InvalidParams The id of the tutor was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 203 API_CODE_INVALID_PARAMS
+ *     {
+ *       "error": "API_CODE_INVALID_PARAMS"
+ *     }
+ */
 module.exports.deleteTutor = (event, context, callback) => {
   // get data from the body of event
   const data = event.body;
